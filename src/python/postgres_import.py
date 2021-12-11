a8c57f287e0efb871709691ffa3a98d8f8af516f
Vitaly R <mortiv16@gmail.com>
2021-12-11 23:10:14 +0300

    adding postgres

---
+++ src/python/postgres_import.py
@@ -0,0 +1,31 @@
+#%%
+# from uuid import UUID
+
+import postgresql
+import pandas as pd
+from sqlalchemy.types import Boolean, Date, String, Integer
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import UUID
+#%%
+# import psycopg2
+
+# conn = psycopg2.connect("dbname=postgres user=postgres password=example port=6001 host=localhost")
+engine = create_engine("postgresql://postgres:example@localhost:6001/postgres")
+# conn = engine.raw_connection()
+#%%
+data = pd.read_csv("/home/ltv/dev/digital-school-analysis/data/db_data/statistic_type_16.11.csv")
+#%%
+# conn.cursor().execute("CREATE SCHEMA IF NOT EXISTS billing;")
+#%%
+data.to_sql(
+    "statistic_types", engine, index=False, if_exists="replace",
+    dtype={
+        "id": UUID,
+        "type_name": String,
+        "type": String,
+        "entity_id": Integer,
+        "updated_at": Date
+    }
+)
+#%%
+conn.commit()
